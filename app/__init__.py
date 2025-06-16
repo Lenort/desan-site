3e92fc257c6520c2fbe7d1d6abe3982d21115c8c
Lenort <98409210+Lenort@users.noreply.github.com>
2025-06-16 12:51:01 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Импортируем маршруты здесь, чтобы они были зарегистрированы на app
-    from app import routes
+    from app.routes import main
+    app.register_blueprint(main)
 
     return app
+
