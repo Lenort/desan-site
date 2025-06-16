9fbff5a8afebc203bce3e426632ac664687f4a73
Lenort <98409210+Lenort@users.noreply.github.com>
2025-06-16 12:50:28 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
-from flask import render_template
+from flask import Blueprint, render_template
 
-@app.route('/')
+main = Blueprint('main', __name__)
+
+@main.route('/')
 def index():
     return render_template('index.html')
